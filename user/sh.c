acf8fbaa2e6301eeed00d9538855c6a82709e123
Syeda Areej Fatima <syedaareejfatima223@gmail.com>
2026-09-15 16:26:55 +0000

    Improve shell file input

--- user/sh.c
+++ user/sh.c
@@ -3,7 +3,7 @@
 #include "kernel/types.h"
 #include "user/user.h"
 #include "kernel/fcntl.h"
-
+#include "kernel/stat.h"
 // Parsed command representation
 #define EXEC  1
 #define REDIR 2
@@ -134,11 +134,17 @@ runcmd(struct cmd *cmd)
 int
 getcmd(char *buf, int nbuf)
 {
-  write(2, "$ ", 2);
+  struct stat st;
+
+  if (fstat(0, &st) >= 0 && st.type == T_DEVICE)
+    write(2, "$ ", 2);
+
   memset(buf, 0, nbuf);
   gets(buf, nbuf);
-  if (buf[0] == 0) // EOF
+
+  if (buf[0] == 0)
     return -1;
+
   return 0;
 }
 
